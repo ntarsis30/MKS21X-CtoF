f75c95c6d12fb9153b09ab8b159c997d16ada786
Nicholas Tarsis <ntarsis30@stuy.edu>
2021-09-24 00:24:42 -0400

    added conversion

--- CtoFTester.java
+++ CtoFTester.java
@@ -4,5 +4,12 @@ public class CtoFTester{
     public static void main (String[] args){
         
     }
+    public double celsiusToFahrenheit(double cels){
+        return (9.0/5)*cels + 32;
+    }
+    public double fahrenheitToCelsius(double fahren){
+        return (9.0/5)*(fahren - 32);
+    }
+
 
 }
